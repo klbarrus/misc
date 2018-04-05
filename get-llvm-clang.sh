731defba500725c901daca8c66b86a38ea772130
Karl Barrus <klbarrus@yahoo.com>
2018-04-04 20:56:05 -0400

    llvm-clang grabber

---
+++ get-llvm-clang.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget http://releases.llvm.org/6.0.0/llvm-6.0.0.src.tar.xz
+wget http://releases.llvm.org/6.0.0/cfe-6.0.0.src.tar.xz
+wget http://releases.llvm.org/6.0.0/compiler-rt-6.0.0.src.tar.xz
+wget http://releases.llvm.org/6.0.0/lld-6.0.0.src.tar.xz
+wget http://releases.llvm.org/6.0.0/lldb-6.0.0.src.tar.xz
+wget http://releases.llvm.org/6.0.0/clang-tools-extra-6.0.0.src.tar.xz
