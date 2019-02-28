1ccaccea837cc8c49f77113d095e4afb7f01c3e9
klbarrus <klbarrus@yahoo.com>
2019-02-27 19:33:33 -0500

    Update setup-cmake.bat
    fix command line args

--- setup-cmake.bat
+++ setup-cmake.bat
@@ -1,5 +1,5 @@
 rd /q /s build-%visualstudioversion%
 mkdir build-%visualstudioversion%
 cd build-%visualstudioversion%
-cmake -G "NMake Makefiles" ..\llvm -DCMAKE_BUILD_TYPE:String=Release -DCLANG_BUILD_EXAMPLES:Bool=On -DLLVM_BUILD_EXAMPLES:Bool=On -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;lld;lldb"
+cmake -G "NMake Makefiles" ..\llvm -DCMAKE_BUILD_TYPE=Release -DCLANG_BUILD_EXAMPLES=On -DLLVM_BUILD_EXAMPLES=On -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;lld;lldb"
 cd ..
