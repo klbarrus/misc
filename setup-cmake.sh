3ba0d0414ebfed35b8bda7390368d2d216df8dea
klbarrus <klbarrus@yahoo.com>
2019-02-28 20:11:57 -0500

    Update setup-cmake.sh
    fix cmake flags

--- setup-cmake.sh
+++ setup-cmake.sh
@@ -3,5 +3,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE:String=Release -DCLANG_BUILD_EXAMPLES:Bool=On -DLLVM_BUILD_EXAMPLES:Bool=On -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;lld;lldb"
+cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Release -DCLANG_BUILD_EXAMPLES=On -DLLVM_BUILD_EXAMPLES=On -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt;lld;lldb"
 cd -
