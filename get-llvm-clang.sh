74d055ea9067112b77daa7673eb8b5a63c7d73f5
Karl Barrus <klbarrus@yahoo.com>
2019-09-21 01:14:04 -0400

    updates for llvm 9.0.0

--- get-llvm-clang.sh
+++ get-llvm-clang.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-wget http://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/llvm-8.0.1.src.tar.xz
-wget http://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/cfe-8.0.1.src.tar.xz
-wget http://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/compiler-rt-8.0.1.src.tar.xz
-wget http://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/lld-8.0.1.src.tar.xz
-wget http://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/lldb-8.0.1.src.tar.xz
-wget http://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/clang-tools-extra-8.0.1.src.tar.xz
+wget http://releases.llvm.org/9.0.0/llvm-9.0.0.src.tar.xz
+wget http://releases.llvm.org/9.0.0/cfe-9.0.0.src.tar.xz
+wget http://releases.llvm.org/9.0.0/compiler-rt-9.0.0.src.tar.xz
+wget http://releases.llvm.org/9.0.0/lld-9.0.0.src.tar.xz
+wget http://releases.llvm.org/9.0.0/lldb-9.0.0.src.tar.xz
+wget http://releases.llvm.org/9.0.0/clang-tools-extra-9.0.0.src.tar.xz
--- setup-llvm-clang.sh
+++ setup-llvm-clang.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 for i in *.xz; do
   tar xJf $i
 done
 
 for i in *.src; do
-  mv "$i" "${i/-8.0.1.src//}"
+  mv "$i" "${i/-9.0.0.src//}"
 done
 
 mv cfe clang
